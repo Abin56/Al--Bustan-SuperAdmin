9c7f62eec15c697177d06ba32f40e92ca419b887
Abin56 <abinjohn8089@gmail.com>
2024-01-01 19:34:51 +0530

    sruthi 1 jan code pushed --@edwin

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: AdminHomeScreen(),
+    //  home: AdminHomeScreen(),
       // home: InventoryWidget()
-     // home: LoginSection(),
+     home: LoginSection(),
     );
   }
 }

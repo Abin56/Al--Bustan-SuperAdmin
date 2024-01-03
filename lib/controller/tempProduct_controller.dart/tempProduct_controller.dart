0cc121c9b1414628a03438dcb5c7ec44c4d750c2
arunvg997733 <arunvg12345@gmail.com>
2024-01-03 20:17:59 +0530

    3 jan - @arunvg - edit function completed

--- lib/controller/tempProduct_controller.dart/tempProduct_controller.dart
+++ lib/controller/tempProduct_controller.dart/tempProduct_controller.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class TempProductController extends GetxController {
+  final fireStore = FirebaseFirestore.instance;
+
   RxBool barcodebool = false.obs;
   RxBool productnamebool = false.obs;
   RxBool categorybool = false.obs;
@@ -10,4 +13,22 @@ class TempProductController extends GetxController {
   RxBool packagetypebool = false.obs;
   RxBool inPricebool = false.obs;
   RxBool outPriceool = false.obs;
+
+  barcodeEdit(String text, String docId) async {
+    final data = {'barcodeNumber': text};
+
+    await fireStore.collection('temporaryCollection').doc(docId).update(data);
+  }
+
+  productNameEdit(String text, String docId) async {
+    final data = {'productname': text};
+
+    await fireStore.collection('temporaryCollection').doc(docId).update(data);
+  }
+
+  productPriceEdit(String text, String docId) async {
+    final data = {'price': text};
+
+    await fireStore.collection('temporaryCollection').doc(docId).update(data);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:canteen_superadmin_website/firebase_options.dart';
+import 'package:canteen_superadmin_website/view/admin_panel/admin_panel.dart';
 import 'package:canteen_superadmin_website/view/widgets/table_listview/table_listview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -18,10 +19,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: TableListviewWidget()),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: AdminHomeScreen());
   }
 }

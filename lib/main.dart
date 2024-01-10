7d1401b39fde70391abfa06b04847df68174f8f2
Abin56 <abinjohn8089@gmail.com>
2024-01-10 18:43:06 +0530

    10 jan arunvg code merging --@edwin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:canteen_superadmin_website/firebase_options.dart';
 import 'package:canteen_superadmin_website/view/admin_panel/inventory/category_widget.dart';
 import 'package:canteen_superadmin_website/view/delivery_panal/delivery_screen/delivery_screen.dart';
 import 'package:canteen_superadmin_website/view/delivery_panal/delivery_screen/prodects.dart';
+import 'package:canteen_superadmin_website/view/login/login_section/login_page.dart';
 import 'package:canteen_superadmin_website/view/welcome_screen/welcome_screen.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,7 +16,17 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-    // webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+  await FirebaseAppCheck.instance
+      .activate(
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+  )
+      .catchError((e) {
+    log(e.toString());
+  });
+
+  await FirebaseAppCheck.instance.getToken().catchError((e) {
+    log(e.toString());
+  });
   runApp(const MyApp());
 }
 
@@ -21,9 +35,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: LoginSection(),
       // home: Scaffold(body: CartWiget()),
     );
   }

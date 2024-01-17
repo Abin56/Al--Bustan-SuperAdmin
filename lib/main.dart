7dfea81619386f3fcbad653f82e33237d753b633
Abin56 <abinjohn8089@gmail.com>
2024-01-17 12:29:02 +0530

    16 jan that day last work --@edwin

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:canteen_superadmin_website/core/colors/colors.dart';
 import 'package:canteen_superadmin_website/firebase_options.dart';
-import 'package:canteen_superadmin_website/view/admin_panel/admin_panel.dart';
-import 'package:canteen_superadmin_website/view/admin_panel/delivery_admin/status_screen/pending_status.dart';
-import 'package:canteen_superadmin_website/view/admin_panel/delivery_admin/status_screen/pending_status/pickuped_status.dart';
-import 'package:canteen_superadmin_website/view/login/login_section/login_page.dart';
+import 'package:canteen_superadmin_website/view/admins/store_Admin/storeadmin_panel.dart';
+import 'package:canteen_superadmin_website/view/admins/super_Admin/super_adminpanel.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -24,10 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: LoginSection(),
-      // home: Scaffold(
-      //     backgroundColor: cWhite,
-      //     body: Center(child: PickedOrdersStatusScreen())),
+      home: SuperAdminPanelScreen(),
     );
   }
 }
